6261776afac5b27b73a60049af7417d7f50a0029
livedhealer <livedhealer16@gmail.com>
2022-04-05 12:14:13 -0700

    Another lesson

--- OrcaMain.py
+++ OrcaMain.py
@@ -5,7 +5,7 @@ from TerraStation import TerraStation
 from selenium import webdriver
 
 #IMPORTANT - Change this to the path for the installed driver on your own machine. I wonder if there's a way to include this in the repo...
-chromeDriverPath = "C:\chromedriver.exe" 
+chromeDriverPath = "./chromedriver.exe"
 
 driver = webdriver.Chrome(chromeDriverPath)
 
