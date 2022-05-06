3fc1357ebb77d40850170c920eeda774187e29af
gman <gillilac19@up.edu>
2022-05-06 16:24:12 -0500

    Exception Handling Added... Also No More Implicit Waiting!
    Sadly need to abandon this approach - need to use Terra Station Browser Extension instead of their website. The website seems to force a user to verify transactions on their phone.

--- Terrastation.py
+++ Terrastation.py
@@ -1,10 +1,13 @@
 # Terrastation.py
 from multiprocessing.connection import wait
+from operator import truediv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+
 import time
 
 """
@@ -20,8 +23,9 @@ Control flow:
 - Swap bluna -> luna -> UST -> aUST
 - Enter password
 -------------Todo------------------
-[] create verifyElementContainsText() function to make sure we're clicking what we think we're clicking...
-[] catch NoSuchElement Exceptions every time we use a driver.find_ method... This'll let us gracefully handle website updates that don't ask us for permission first.
+[x] create verifyElementContainsText() function to make sure we're clicking what we think we're clicking...
+[x] catch NoSuchElement Exceptions every time we use a driver.find_ method... This'll let us gracefully handle website updates that move buttons around on us...
+[x] instead of .implicitly_wait method calls, use loops or builtin functions that watch for something to populate...
 """
 
 
@@ -31,35 +35,56 @@ class TerraStation:
     def __init__(self):
         self.driver = webdriver.Chrome(".\chromedriver.exe")
 
-        #Get the driver pointed towards the Terra Station swap webpage...
+        #User creds stored for plugging into website prompts when completing the swaps...
+        self.username = ""
+        self.password = ""
         self.terraStationURL = "https://station.terra.money/swap" 
+
+        #Get the driver pointed towards the Terra Station swap webpage...
         self.driver.get(self.terraStationURL)
  
-        self.driver.implicitly_wait(5) #Identified as the method to get button to be clicked
+        # self.driver.implicitly_wait(7) #Identified as the method to get button to be clicked
+        # connect = self.driver.find_element_by_xpath("/html/body/div[1]/div/header/div/div/button")
+        # connect.click()
+        try:
+            connect = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/header/div/div/button")))
+            self.verifyElementContainsText(connect, "Connect")
+            connect.click()
+        except TimeoutException:
+            print("Red alert! Either item does not exist or internet connection timed out.")
  
-        connect = self.driver.find_element_by_xpath("/html/body/div[1]/div/header/div/div/button")
-        connect.click()
  
-        self.driver.implicitly_wait(1) #Identified as the method to get button to be clicked
- 
-        walletConnect = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/section/div/section/button[2]")
-        walletConnect.click()
+        try:
+            walletConnect = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[4]/div/div/section/div/section/button[2]")))
+            self.verifyElementContainsText(walletConnect, "Wallet Connect")
+            walletConnect.click()
+        except TimeoutException:
+            print("Red alert! Either item does not exist or internet connection timed out.")
 
         self.driver.implicitly_wait(1) #Identified as the method to get button to be clicked
 
         #Wait while the QR code popup is on screen... 
-        popUpOnScreen = True
-        while popUpOnScreen:
+        popupOnScreen = True
+        while popupOnScreen:
             try:
-                self.driver.find_element_by_class_name("wallet-wc-modal--content")
+                popup = self.driver.find_element_by_class_name("wallet-wc-modal--content")
             except NoSuchElementException:
-                print("Successfully waited for user to connect")
-                popUpOnScreen = False
+                popupOnScreen = False
 
-        #User creds stored for plugging into website prompts when completing the swaps...
-        self.username = ""
-        self.password = ""
+        connect = self.driver.find_element_by_xpath("/html/body/div[1]/div/header/div/div/button") #
 
+
+#Make sure the webElement contains the text specified before proceeding. If not, kill the program. 
+# In the future, it would be very nice to get a phone alert under these circumstances...
+    def verifyElementContainsText(self, webElement, text):
+        if(webElement.text.__contains__(text)):
+            print(webElement.text + " contains " + text)
+            return 
+        else:
+            print("Red alert! " + webElement.text + " does not contain " + text)
+            exit()
+            return
+            
 
     def showAtrributes(self):
         print(self.terraStationURL,self.username, self.password)
