7b280b47e50cf9200ed7dc4f59c2344ca47a2aa7
livedhealer <livedhealer16@gmail.com>
2022-05-21 10:47:34 -0700

    Updated Kujira_testing
    Copied Orca main for testing environment

--- Kujira_testing.py
+++ Kujira_testing.py
@@ -1,3 +1,14 @@
 from Kujira import Kujira
 
-Run=Kujira()
+loginInfoPath = input("Enter filepath to your loginfile something like that: ")
+
+orca = Kujira(loginInfoPath)
+input("Press enter when ready") #Need to use this time to manually click the "add wallet buttons"
+
+# # When fully uncommented, this loop will run until you stop it - making money the whole time...
+
+bLunaWithdrawalAmount = orca.autoWithdraw()
+orca.authorizeTransaction()
+
+print("I made it here")
+
